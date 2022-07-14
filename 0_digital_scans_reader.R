297d65c8926a8edd5ed1d8078b5bee58524a2c46
Kyle Belanger <kyleb44@hotmail.com>
2022-07-13 22:48:55 -0400

    back to normal

--- 0_digital_scans_reader.R
+++ 0_digital_scans_reader.R
@@ -36,27 +36,31 @@ drive_auth(path = Sys.getenv("GOOGLE_JSON"))
 # load google sheet data --------------------------------------------------
 
 # Load data
-# MD_VDM_raw <- googlesheets4::read_sheet("1lErAsk3znF6a1rcRvrqwXxwJWSFx4Z07L0cqLSo_H_c") %>%   
-#   dplyr::select(
-#   date = Timestamp
-#   ,SWC = `Email Address`
-#   ,labs_visited = `How many labs did you visit in the last two weeks?`
-#   ,labs_scanned = `How many labs did you scan with Matterport in the last two weeks?`) %>% 
-#   dplyr::mutate(  #turn Email address into SWC name
-#     across(SWC, ~str_extract(., "^.*(?=(@))") )
-#     ,across(SWC, ~str_remove(.,"\\.jw1"))  #should figure out a way to make this reusable
-#     ,across(SWC, ~str_replace_all(., "\\.", " "))
-#     ,across(SWC, ~snakecase::to_title_case(.))
-#   ) %>% 
-#   dplyr::mutate( #convert timestamp to date 
-#     across(date, ~lubridate::as_date(.))
-#   ) %>%
-#   dplyr::mutate( #fix issue with incorrect amount of labs visited
-#     across(labs_visited, ~if_else(labs_visited < labs_scanned, labs_scanned, labs_visited ))
-#   ) %>% 
-#   dplyr::mutate(
-#     manager = 'Todd Ward'
-#   )
+MD_VDM_raw <- googlesheets4::read_sheet("1lErAsk3znF6a1rcRvrqwXxwJWSFx4Z07L0cqLSo_H_c")
+
+
+MD_VDM_ds1 <- MD_VDM_raw %>% 
+  select(
+    date = Timestamp
+    ,SWC = `Email Address`
+    ,labs_visited = `How many labs did you visit in the last two weeks?`
+    ,labs_scanned = `How many labs did you scan with Matterport in the last two weeks?`
+    ) %>%
+  dplyr::mutate(  #turn Email address into SWC name
+    across(SWC, ~str_extract(., "^.*(?=(@))") )
+    ,across(SWC, ~str_remove(.,"\\.jw1"))  #should figure out a way to make this reusable
+    ,across(SWC, ~str_replace_all(., "\\.", " "))
+    ,across(SWC, ~snakecase::to_title_case(.))
+  ) %>%
+  dplyr::mutate( #convert timestamp to date
+    across(date, ~lubridate::as_date(.))
+  ) %>%
+  dplyr::mutate( #fix issue with incorrect amount of labs visited
+    across(labs_visited, ~if_else(labs_visited < labs_scanned, labs_scanned, labs_visited ))
+  ) %>%
+  dplyr::mutate(
+    manager = 'Todd Ward'
+  )
 
 
 
@@ -64,28 +68,22 @@ drive_auth(path = Sys.getenv("GOOGLE_JSON"))
 
 
 
-cd_vdm_form_raw <- get_sheet_as_csv(Sys.getenv("SMARTSHEET_VDM_ID")) %>% read_csv() %>% 
-  filter(!is.na("1st Onsite LPA")) %>% 
-  rename("date" = "1st Onsite LPA"
-         ,"SWC" = "Primary SWC"
-         ,"manager" = "SWC Manager"
-         ,"scan" = "Lab Measurement?")
-
-cat("The first part of this worked!")
-
-
-  # select(
-  #   "date" = "1st Onsite LPA"
-  #   ,"SWC" = "Primary SWC"
-  #   ,"manager" = "SWC Manager"
-  #   ,"scan" = "Lab Measurement?"
-  # ) %>% 
-  # mutate(
-  #   across("date", ~lubridate::mdy(.))
-  #   ,across("scan", ~replace_na(.,"No Data"))
-  #   ,"labs_scanned" = if_else(str_detect("scan","Digitally"),1,0)
-  #   ,"labs_visited" = 1
-  # )
+cd_vdm_form_raw <- get_sheet_as_csv(Sys.getenv("SMARTSHEET_VDM_ID")) %>% read_csv()
+  
+CD_VDM_ds1 <- cd_vdm_form_raw %>%   
+  filter(!is.na(`1st Onsite LPA`)) %>% 
+  select(
+    "date" = "1st Onsite LPA"
+    ,"SWC" = "Primary SWC"
+    ,"manager" = "SWC Manager"
+    ,"scan" = "Lab Measurement?"
+  ) %>%
+  mutate(
+    across("date", ~lubridate::mdy(.))
+    ,across("scan", ~replace_na(.,"No Data"))
+    ,"labs_scanned" = if_else(str_detect("scan","Digitally"),1,0)
+    ,"labs_visited" = 1
+  )
 
 # combine data  -----------------------------------------------------------
 
